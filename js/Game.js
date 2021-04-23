ed849b170fa6606f90bb0087ffe3020ca59a4f75
jamie1077 <jm1077@sky.com>
2021-04-23 19:40:33 +0100

    built initial classes and methods to start game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,49 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(){
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases(){
+        const phrases = [
+            new Phrase('May the Force be with you'), 
+            new Phrase('Live long and prosper'),
+            new Phrase('Houston we have a problem'),
+            new Phrase('Nobody puts Baby in a corner'),
+            new Phrase('Thats why they call it the jungle sweetheart')
+        ]
+        
+        return phrases;
+    }
+
+    
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase(){
+        const randomPhase = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomPhase];
+    }
+
+
+    /**
+    * Begins game by selecting a random phrase and displaying it to user
+    */
+    startGame() {
+        const overlay = document.querySelector('#overlay');
+        overlay.style.display = 'none';
+
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+
+
+    /**
+    * Display phrase on game board
+    * This method adds letter placeholders to the display when the game starts. 
+    * Each letter is presented by an empty box, one list item for each letter
+    */
+    addPhraseToDisplay(){
+        const phraseList = document.querySelector('#phrase ul');
+        const letters = this.phrase.split('');
+        let html = ``;
+
+        letters.forEach(letter => {
+            if(letter !== ' '){
+                html += `<li class="hide letter ${letter}">${letter}</li>`;
+            }else{
+                html += `<li class="space">${letter}</li>`;
+            }
+        });
+        phraseList.innerHTML = html;
+    }
+
+    checkLetter(){
+
+    }
+
+    showMatchedLetter(){
+
+    }
+}
